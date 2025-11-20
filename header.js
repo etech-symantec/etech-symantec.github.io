4966fcadc6e2154aeabacc105bfd4df944a5b661
etech-symantec <shin86@etechsystem.co.kr>
2025-11-20 13:19:03 +0900

    Update header.js

--- header.js
+++ header.js
@@ -31,7 +31,8 @@ document.addEventListener("DOMContentLoaded", function() {
           <span class="subtitle">by 이테크시스템</span>
         </div>
         <div class="date-container">
-          <div class="version">${version}</div><p>
+          <div class="version">${version}</div>
+          <div class="date-header" id="today-date"></div>
         </div>
       </h1>
     </div>
