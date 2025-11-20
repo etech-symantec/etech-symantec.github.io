9d63143149e640917a9dd05131357ceb3d3bceab
etech-symantec <shin86@etechsystem.co.kr>
2025-11-20 15:47:03 +0900

    Update header.js

--- header.js
+++ header.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
         </div>
       </div>
 
-      <h1 class="sunflower-light">
+      <h1 class="gamja-flower-regular">
         <div class="title-left">
           ${title}<br>
           <span class="subtitle">by 이테크시스템</span>
